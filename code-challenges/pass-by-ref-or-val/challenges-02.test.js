6c1a41ee0cd151deb7b8876ccdd766dbc1655c15
Gina Pultorak <gina.pultorak@gmail.com>
2020-01-07 17:39:35 -0800

    code challenge 2 pass by value or reference complete

--- code-challenges/pass-by-ref-or-val/challenges-02.test.js
+++ code-challenges/pass-by-ref-or-val/challenges-02.test.js
@@ -10,6 +10,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 
 const appendTheEnd = (str) => {
   // Solution code here...
+  let modStr = str + ' The end.';
+  return modStr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -27,6 +29,7 @@ console.log(a) prints [1, 2, 3, 1]
 
 const appendFirstToLast = (arr) => {
   // Solution code here...
+  arr.push(arr[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -46,6 +49,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -63,6 +67,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  people.forEach( person => {
+    person.isAuthor = true;
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -82,7 +89,9 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-
+  arr2.forEach( value => {
+    arr1.push(value);
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
